1af646fd60238d33c00c73b768014094e5d731a1
mfarouq122 <monaaliali01@gmail.com>
2026-03-28 19:37:38 +0200

    Update Pi-complete.js

--- netlify/functions/Pi-complete.js
+++ netlify/functions/Pi-complete.js
@@ -1,5 +1,12 @@
-exports.handler = async function(event, context) {
-  const body = JSON.parse(event.body);
+exports.handler = async function(event) {
+
+  let body = {};
+
+  try {
+    body = JSON.parse(event.body || "{}");
+  } catch (e) {
+    body = {};
+  }
 
   console.log("Complete:", body.paymentId, body.txid);
 
