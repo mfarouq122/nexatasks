9f572975541bf50ce9e58615948167e6fd819519
mfarouq122 <monaaliali01@gmail.com>
2026-03-28 19:36:39 +0200

    Update Pi-cancel.js

--- netlify/functions/Pi-cancel.js
+++ netlify/functions/Pi-cancel.js
@@ -1,10 +1,17 @@
-exports.handler = async function(event, context) {
-  const body = JSON.parse(event.body);
+exports.handler = async function(event) {
 
-  console.log("Cancel:", body.paymentId);
+  let body = {};
+
+  try {
+    body = JSON.parse(event.body || "{}");
+  } catch (e) {
+    body = {};
+  }
+
+  console.log("Approve:", body.paymentId);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ result: "cancelled" })
+    body: JSON.stringify({ result: "approved" })
   };
 };
